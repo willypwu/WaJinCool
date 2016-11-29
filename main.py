9e74c09f57b59729eb3db4f5bbf73d320f991d09
Willy Wu <willypwu@gmail.com>
2016-11-29 19:00:33 +0800

    [Server][Client]Category function

--- main.py
+++ main.py
@@ -37,7 +37,10 @@ class MoneyRecord(ndb.Model):
     
     @classmethod
     def query_record_by_name(cls, user_name):
-        return cls.query(MoneyRecord.name==user_name).order(cls.user_date)
+        return cls.query(MoneyRecord.name==user_name).order(cls.user_date)    
+    @classmethod
+    def query_record_by_name_and_category(cls, user_name, cate):
+        return cls.query(ndb.AND(MoneyRecord.name==user_name, MoneyRecord.category==cate)).order(cls.user_date)
 
 class CategoryOptions(ndb.Model):
     name = ndb.StringProperty()
@@ -65,22 +68,29 @@ class MainPage(webapp2.RequestHandler):
 class MoneyRecordHandler(webapp2.RequestHandler):
     def get(self):         
         user = users.get_current_user()
-        logging.info("*** MoneyRecordHandler get *** " + str(user))
+        select_year = self.request.get('select_year')
+        select_month = self.request.get('select_month')
+        logging.info("*** MoneyRecordHandler get *** " + str(user) + ", " + select_year + ", " + select_month)
         records = MoneyRecord.query_record_by_name(str(user)).fetch()
         content = []
+        count = 0
+        filter_year_and_month = str(select_year+"-"+select_month)
         for record in records:
             logging.info("record // name = %s , date = %s, money = %d, category = %s, comment =%s, id = %s",
                          record.name, record.user_date, record.money, record.category, record.comment, record.key)
-            content.append({
-                "id":record.key.urlsafe(), 
-                "date": record.user_date, 
-                  "cost": record.money, 
-                  "category": record.category,
-                  "comment": record.comment
-            })
+            if filter_year_and_month in record.user_date :
+                logging.info("matched data : " + record.user_date)
+                content.append({
+                    "id":record.key.urlsafe(), 
+                    "date": record.user_date, 
+                      "cost": record.money, 
+                      "category": record.category,
+                      "comment": record.comment
+                })
+                count = count + 1
         result = {
             "user":str(user),
-            "count":len(records), 
+            "count":count, 
             "content":content
         }
         json_obj = json.dumps(result)
@@ -184,6 +194,52 @@ class CategoryHandler(webapp2.RequestHandler):
         json_obj = json.dumps(result)
         self.response.out.write(json_obj)
         
+class MoneyStatisticsHandler(webapp2.RequestHandler):
+    def get(self): 
+        user = users.get_current_user()
+        logging.info("*** MoneyStatisticsHandler get *** " + str(user))
+        # year+month
+        select_year = self.request.get('select_year')
+        select_month = self.request.get('select_month')
+        filter_year_and_month = str(select_year+"-"+select_month)        
+        # get all cates
+        user_cat = CategoryOptions.query_categories_by_name(str(user)).fetch()        
+        split_categories = user_cat[0].categories.split(";")
+        # get corresponding record
+        categories_data = []
+        in_count = 0
+        out_count = 0
+        for c in split_categories:
+            split_cate = c.encode('utf-8').split(".")
+            records = MoneyRecord.query_record_by_name_and_category(str(user), split_cate[1]).fetch()
+            money_count = 0
+            for record in records :
+                if filter_year_and_month in record.user_date :
+                    money_count = money_count + record.money 
+                    # in or out
+                    if split_cate[0]=='in' :
+                        in_count = in_count + record.money 
+                    else :
+                        out_count = out_count + record.money 
+            # save as categories_data
+            categories_data.append({
+                "cate":c.encode('utf-8'),
+                "money_count":money_count
+            })
+                
+                         
+        result = {
+            "user":str(user),
+            "in_count":in_count,
+            "out_count":out_count,
+            "count":len(split_categories),
+            "categories_data":categories_data
+        }
+        logging.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" + str(result))
+        json_obj = json.dumps(result)
+        self.response.out.write(json_obj)
+        
+        
 
         
 app = webapp2.WSGIApplication([
@@ -191,4 +247,5 @@ app = webapp2.WSGIApplication([
     ('/moneyHandle', MoneyRecordHandler),
     ('/moneyHandle/([^/]+)?', MoneyRecordHandler),
     ('/categories', CategoryHandler),
+    ('/statistics', MoneyStatisticsHandler),
 ], debug=True)
